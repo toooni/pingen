85922241c0ef4754eb8cbb78472f7e8c87f891fb
pingencom <info@pingen.com>
2013-03-26 13:15:18 +0100

    Adapted License to 3-clause BSD license

--- Pingen.php
+++ Pingen.php
@@ -1,7 +1,7 @@
     <?php
 
     /**
-     * A class to use the API of pingen.com as an integrator (Version 1.01)
+     * A class to use the API of pingen.com as an integrator (Version 1.02)
      *
      * For more information about Pingen and how to use it as an integrator see
      * https://pingen.com/en/customer/integrator/Briefversand-für-Integratoren.html
@@ -10,22 +10,30 @@
      * https://www.pingen.com/en/developer.html
      *
      *
-     * Copyright (c) 2013 by Pingen.com
-     * Permission is hereby granted, free of charge, to any person obtaining a
-     * copy of this software and associated documentation files (the "Software"),
-     * to deal in the Software without restriction, including without limitation
-     * the rights to use, copy, modify, merge, publish, distribute, sublicense,
-     * and/or sell copies of the Software, and to permit persons to whom the
-     * Software is furnished to do so, subject to the following conditions:
-     * The above copyright notice and this permission notice shall be included
-     * in all copies or substantial portions of the Software.
-     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-     * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-     * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-     * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-     * OR OTHER DEALINGS IN THE SOFTWARE.
+     *  Copyright (c) 2013, Pingen GmbH
+     *  All rights reserved.
+     *
+     *  Redistribution and use in source and binary forms, with or without
+     *  modification, are permitted provided that the following conditions are met:
+     *   * Redistributions of source code must retain the above copyright
+     *  notice, this list of conditions and the following disclaimer.
+     *   * Redistributions in binary form must reproduce the above copyright
+     *  notice, this list of conditions and the following disclaimer in the
+     *  documentation and/or other materials provided with the distribution.
+     *   * Neither the name of the <organization> nor the
+     *  names of its contributors may be used to endorse or promote products
+     *  derived from this software without specific prior written permission.
+     *
+     *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+     *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+     *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+     *  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+     *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+     *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+     *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+     *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+     *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+     *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
      *
      * @link https://www.pingen.com/en/developer.html
      */
@@ -46,7 +54,7 @@
         /**
          * @constant string Library-Version
          */
-        const VERSION = 1.01;
+        const VERSION = 1.02;
 
         /**
          * @var string Base URL of Pingen API
